2b3fee273f2e2360f0602c8cde8771908120683b
Abhyanshu Pandey <Abhyanshu Pandey>
2023-10-08 11:13:10 +0530

    login and register page done

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './login.scss'
+
+const Login = () => {
+    return (
+        <div className='login'>
+            <div className='card'>
+                <div className='left'>
+                    <h1>Hello World.</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Corporis saepe et eum eveniet repudiandae voluptas, commodi
+                        rerum vitae officia minus incidunt quo, iure consectetur
+                        vero eaque, accusantium repellat? Perspiciatis, officia!
+                    </p>
+                    <span>Do You have and account?</span>
+                    <button>Register</button>
+                </div>
+                <div className='right'>
+                  <h1>Login</h1>
+                  <form>
+                    <input type="text" placeholder='Username' />
+                    <input type='password' placeholder='Password'/> 
+                    <button>Login</button>
+                  </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './register.scss'
+
+const Register = () => {
+  return (
+    <div className='register'>
+            <div className='card'>
+                <div className='left'>
+                    <h1>Abhy Social</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Corporis saepe et eum eveniet repudiandae voluptas, commodi
+                        rerum vitae officia minus incidunt quo, iure consectetur
+                        vero eaque, accusantium repellat? Perspiciatis, officia!
+                    </p>
+                    <span>Do You have and account?</span>
+                    <button>Login</button>
+                </div>
+                <div className='right'>
+                  <h1>Register</h1>
+                  <form>
+                    <input type="text" placeholder='Username' />
+                    <input type='password' placeholder='Password'/> 
+                    <input type='email' placeholder='Email'/> 
+                    <input type='text' placeholder='Name'/> 
+                    <button>Register</button>
+                  </form>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Register;
