2804e9ef2cb412403c768fd96235c7e252be9ecd
Abhyanshu Pandey <Abhyanshu Pandey>
2023-10-09 10:23:11 +0530

    left bar and right bar is created - till the static data

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -2,11 +2,110 @@ import React from 'react'
 import './rightBar.scss'
 
 const RightBar = () => {
-  return (
-    <div>
-      Right Bar
-    </div>
-  )
+    return (
+        <div className='rightBar'>
+            <div className="container">
+                <div className="item">
+                    <span>Suggestions For you
+                    </span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <span>John Doe</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Follow</button>
+                            <button>Dismiss</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <span>John Doe</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Follow</button>
+                            <button>Dismiss</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Latest Activities</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <p>
+                                <span>John Doe</span>
+                                This is the p tag
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <p>
+                                <span>John Doe</span>
+                                This is the p tag
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <p>
+                                <span>John Doe</span>
+                                This is the p tag
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <p>
+                                <span>John Doe</span>
+                                This is the p tag
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Online Friends</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <div className="online" />
+                            <span>John Doe</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <div className="online" />
+                            <span>John Doe</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <div className="online" />
+                            <span>John Doe</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                            <div className="online" />
+                            <span>John Doe</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 
