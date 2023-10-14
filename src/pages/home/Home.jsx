9c79a718a4be3b27b23d0a13e9121a8e929a4e67
Abhyanshu Pandey <abhyanshupandeyji@gmail.com>
2023-10-14 10:59:49 +0530

    post showing section complete , done the post where we can access posts , and can show the real time data , instead of using the useEffect and redux , we use react-query library , and also created the base url , with the help of axios , and also the url endpoint still have a / after , even though in requests you need to put the / before the api hit request , weird but ok , faced many problem in the way , import problem , and data access problem , and rem only put the required arguments in the function , the req ,and res are predefiend for the .then() function

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './home.scss'
 import Stories from '../../components/stories/Stories'
 import Posts from '../../components/posts/Posts.jsx'
+import Share from '../../components/share/Share.jsx'
 
 const Home = () => {
   return (
     <div className='home'>
       <Stories/>
+      <Share/>
       <Posts/>
     </div>
   )
