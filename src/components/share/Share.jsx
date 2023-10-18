34db3ec701a57ba18b86db9230e31d678353c4cb
Abhyanshu Pandey <abhyanshupandeyji@gmail.com>
2023-10-18 16:25:02 +0530

    adding posts also completed using react-query and mutation , fetcing and adding new data

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -2,12 +2,47 @@ import "./share.scss";
 import Image from "../../assets/img.png";
 import Map from "../../assets/map.png";
 import Friend from "../../assets/friend.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
+
+
+import {
+  useMutation,
+  useQueryClient,
+} from 'react-query';
 
 const Share = () => {
 
-  const {currentUser} = useContext(AuthContext)
+
+  const {currentUser} = useContext(AuthContext);
+
+  const queryClient = useQueryClient();
+
+
+  const [file , setFile] = useState(null);
+  const [desc , setDesc] = useState("")
+
+
+  // adding data and fetching data using react query
+  const mutation = useMutation((newPost)=>{
+    return makmutationeRequest.post('/posts' , newPost);
+    },  
+    {
+      onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries(["posts"])
+    },
+  })
+
+  const handleClick = (e) =>{
+    e.preventDefault();
+    // we will use react-query use mutation for creating our post requests
+    mutation.mutate({
+      desc
+    })
+  
+  }
+
   return (
     <div className="share">
       <div className="container">
@@ -16,12 +51,14 @@ const Share = () => {
             src={currentUser.profilePic}
             alt=""
           />
-          <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} />
+          <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} 
+          onChange={(e)=>setDesc(e.target.value)}/>
         </div>
         <hr />
         <div className="bottom">
           <div className="left">
-            <input type="file" id="file" style={{display:"none"}} />
+            <input type="file" id="file" style={{display:"none"}} 
+            onChange={(e)=>setFile(e.target.files[0])}/>
             <label htmlFor="file">
               <div className="item">
                 <img src={Image} alt="" />
@@ -38,7 +75,7 @@ const Share = () => {
             </div>
           </div>
           <div className="right">
-            <button>Share</button>
+            <button onClick={handleClick}>Share</button>
           </div>
         </div>
       </div>
